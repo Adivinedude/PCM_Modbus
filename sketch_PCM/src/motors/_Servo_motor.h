55c19d44b4a32d94023458076c57906dce352c12
Adivinedude <151443912+Adivinedude@users.noreply.github.com>
2023-12-27 20:12:04 -0500

    Delete sketch_PCM/src/motors directory

--- sketch_PCM/src/motors/_Servo_motor.h
+++
@@ -1,7 +0,0 @@
-#ifndef ___SERVO_MOTOR_H__
-  #define ___SERVO_MOTOR_H__
-  void Servo_setup(unsigned char motor_number);
-  void Servo_run(unsigned char motor_number, short);
-  void Servo_work(unsigned char motor_number);
-  void Servo_stop(unsigned char motor_number);
-#endif
